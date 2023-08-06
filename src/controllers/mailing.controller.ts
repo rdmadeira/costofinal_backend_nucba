00b9af6e468069635de5b023ec1c1952ac6f4135
rdmadeira <rdmadeira2@gmail.com>
2023-08-06 12:50:24 -0300

    ok to deploy in Heroku

--- src/controllers/mailing.controller.ts
+++ src/controllers/mailing.controller.ts
@@ -9,6 +9,7 @@ import { sendMail } from '../nodemailer/utils.js';
 import { ServerError } from '../entities/errors/ServerError.js';
 import { BadRequestError } from '../entities/errors/BadRequestError.js';
 import mongoose from 'mongoose';
+import SMTPTransport from 'nodemailer/lib/smtp-transport/index.js';
 
 export const mailingController = async (
   req: Request,
@@ -25,16 +26,19 @@ export const mailingController = async (
     try {
       const sendMailOptions = sendMail({ ...mailData, email: user.email });
 
-      transporter.sendMail(sendMailOptions, (error: any, info: any) => {
-        if (error) {
-          console.log('error', error);
-          return error;
+      transporter.sendMail(
+        sendMailOptions,
+        (error: Error | null, info: SMTPTransport.SentMessageInfo) => {
+          if (error) {
+            console.log('error', error);
+            return next(error);
+          }
+          if (info) {
+            console.log('info', info);
+            return res.status(200).json({ data: info });
+          }
         }
-        if (info) {
-          console.log('info', info);
-          return res.status(200).json({ data: info });
-        }
-      });
+      );
     } catch (error) {
       console.log('errorerror', error);
     }
